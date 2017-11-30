c7fda8d046d4dfbb7ab7b1995cd9545fcd057360
Maxxxiimus92 <ulrichhuet@hotmail.com>
2017-11-30 19:31:51 +0100

    Added favicon

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Votre message a bient été envoyé. </strong>");
+            .append("<strong>Votre message a bien été envoyé. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
--- views/layout.php
+++ views/layout.php
@@ -2,108 +2,110 @@
 <html lang="fr">
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="Projet Blog pour OpenClassrooms - Parcours développeur d'application - PHP / Symfony">
-	<meta name="author" content="Ulrich Huet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Projet Blog pour OpenClassrooms - Parcours développeur d'application - PHP / Symfony">
+    <meta name="author" content="Ulrich Huet">
 
-	<title><?= $title ?></title>
+    <title><?= $title ?></title>
 
-	<!-- Bootstrap core CSS -->
-	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="favicon.png" />
 
-	<!-- Custom fonts for this template -->
-	<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-	<!-- Custom styles for this template -->
-	<link href="css/clean-blog.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/clean-blog.min.css" rel="stylesheet">
 </head>
 
 <body>
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-		<div class="container">
-		<a class="navbar-brand" href="index.php">Projet Blog</a>
-		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-		  Menu
-			<i class="fa fa-bars"></i>
-		</button>
-			<div class="collapse navbar-collapse" id="navbarResponsive">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="index.php">Accueil</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="index.php?p=list">Articles</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="index.php?p=add">Ajouter</a>
-					</li>
-				</ul>
-			</div>
-		</div>
+        <div class="container">
+        <a class="navbar-brand" href="index.php">Projet Blog</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+            <i class="fa fa-bars"></i>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto" style="text-align:center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?p=list">Articles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?p=add">Ajouter</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
     
     <!-- Page Header -->
-	<header class="masthead" style="background-image: url('img/computer.jpg')">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-10 col-md-10 mx-auto">
-					<div class="site-heading">
-						<?= $header ?>
-						<?= $subheader ?>
-						<br>
-						<?= $button ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
-	
-	<!-- Content -->
-	<div class="container">
-		<?= $content ?>
-	</div>
-	
-	<hr>
-	
+    <header class="masthead" style="background-image: url('img/computer.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-md-10 mx-auto">
+                    <div class="site-heading">
+                        <?= $header ?>
+                        <?= $subheader ?>
+                        <br>
+                        <?= $button ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <!-- Content -->
+    <div class="container">
+        <?= $content ?>
+    </div>
+    
+    <hr>
+    
     <!-- Footer -->
     <footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-8 col-md-10 mx-auto">
-					<ul class="list-inline text-center">
-						<li class="list-inline-item">
-							<a href="https://www.twitter.com/Maxxximus92" target="_blank">
-								<span class="fa-stack fa-lg">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-								</span>
-							</a>
-						</li>
-						<li class="list-inline-item">
-							<a href="https://www.linkedin.com/in/ulrichhuet" target="_blank">
-								<span class="fa-stack fa-lg">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
-								</span>
-							</a>
-						</li>
-						<li class="list-inline-item">
-							<a href="https://www.github.com/Maxxxiimus92/p5_blog" target="_blank">
-								<span class="fa-stack fa-lg">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-github fa-stack-1x fa-inverse"></i>
-								</span>
-							</a>
-						</li>
-					</ul>
-					<p class="copyright text-muted">Copyright &copy; Projet Blog 2017</p>
-				</div>
-			</div>
-		</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <ul class="list-inline text-center">
+                        <li class="list-inline-item">
+                            <a href="https://www.twitter.com/Maxxximus92" target="_blank">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://www.linkedin.com/in/ulrichhuet" target="_blank">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://www.github.com/Maxxxiimus92/p5_blog" target="_blank">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; Projet Blog 2017</p>
+                </div>
+            </div>
+        </div>
     </footer>
 
     <!-- Bootstrap core JavaScript -->
@@ -111,7 +113,7 @@
     <script src="vendor/popper/popper.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
-	<!-- Contact Form JavaScript -->
+    <!-- Contact Form JavaScript -->
     <script src="js/jqBootstrapValidation.js"></script>
     <script src="js/contact_me.js"></script>
 
